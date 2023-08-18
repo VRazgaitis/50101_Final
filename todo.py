380e68b92a4311e269a49e4e678a3bc7fec5327a
VRazgaitis <vaidas.razgaitis@gmail.com>
2023-08-17 20:06:33 -0400

    TODO next: add pickling functionality

--- todo.py
+++ todo.py
@@ -3,6 +3,7 @@ Task Manager Program by Vaidas Razgaitis
 """
 import argparse
 import datetime
+import sys
 
 
 class Task:
@@ -10,13 +11,14 @@ class Task:
     # TODO: unique identifier when a task has been instantiated
     #   -use a generator for this?
 
-    def __init__(self, name, priority, completed=None, due=None):
+    def __init__(self, name, priority, unique_id, completed=None, due=None):
         self.created = datetime.datetime.now()
         self.completed = completed
         self._parse_date_string()
         self.name = name
         self.priority = priority if priority else 1
         self.due = due
+        self.id = unique_id
 
     def _parse_date_string(self):
         """
@@ -30,10 +32,11 @@ class Task:
 class Tasks:
     """A list of `Task` objects."""
 
-    def __init__(self):
+    def __init__(self, task_info=None):
         """Read pickled tasks file into a list"""
         # List of Task objects
         self.tasks = []
+        # self.task_data = task_info
 
     def pickle_tasks(self):
         """Picle your task list to a file"""
@@ -51,30 +54,49 @@ class Tasks:
     def query(self):
         pass
 
-    def add(self):
-        pass
+    def add(self, task_info):
+        self.tasks.append(task_info)
 
 
-# Create the parser
-parser = argparse.ArgumentParser()
+def command_valid(user_action):
+    """Ensure that the user has chosen a valid action verb"""
+    # Only 6 approved verbs
+    action_options = ['--add',
+                      '--delete',
+                      '--list',
+                      '--report',
+                      '--query',
+                      '--done']
 
-# Add name argument
-parser.add_argument('--name', type=str, required=True)
+    if user_action not in action_options:
+        print('There was an error in choosing TODO list actions. Run "todo -h" for usage instructions.')
+        return False
+    else:
+        return True
 
-# Add due date argument
-parser.add_argument('--due', type=str, required=False)
 
-# Add priority argument
-parser.add_argument('--priority', type=str, required=False)
+if __name__ == "__main__":
+    # get the user's action verb
+    user_action = (sys.argv[1])
 
-# Parse the argument
-args = parser.parse_args()
+    if command_valid(user_action):
+        # Create parser
+        parser = argparse.ArgumentParser()
+        tasklist = Tasks()
 
-task_1 = Task(name=args.name,
-              priority=args.priority,
-              due=args.due)
+        if user_action == '--add':
+            # Add name argument
+            parser.add_argument('--add', type=str, required=False)
+            # Add due date argument
+            parser.add_argument('--due', type=str, required=False)
+            # Add priority argument
+            parser.add_argument('--priority', type=str, required=False)
+            args = parser.parse_args()
 
-print(f'task name: {task_1.name}')
-print(f'task due date: {task_1.due}')
-print(f'task priority: {task_1.priority}')
-print(f'task created: {task_1.created}')
+            # Instantiate a task object
+            new_task = Task(name=args.add,
+                            priority=args.priority,
+                            due=args.due,
+                            unique_id=len(tasklist.tasks) + 1)
+
+            tasklist.add(new_task)
